af1daa2e70d3536aa66faccfc7887b3c4df86ae2
Matt Gifford <me@mattgifford.co.uk>
2015-01-30 14:40:33 +0000

    Additional Lucee addons to build process

--- tests/ci/scripts/ci-helper-lucee42.sh
+++ tests/ci/scripts/ci-helper-lucee42.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 case $1 in
 	start)
-		CONTROL_SCRIPT='lucee/start'
+		CONTROL_SCRIPT='lucee/start.sh'
 		;;
 	stop)
-		CONTROL_SCRIPT='lucee/stop'
+		CONTROL_SCRIPT='lucee/stop.sh'
 		;;
 esac
 
@@ -15,12 +15,12 @@ source $MY_DIR/ci-helper-base.sh $1 $2
 
 case $1 in
 	install)
-		chmod a+x lucee/start
-		chmod a+x lucee/stop
+		chmod a+x lucee/start.sh
+		chmod a+x lucee/stop.sh
 
-		sed -i "s/jetty.port=8888/jetty.port=$SERVER_PORT/g" lucee/start
-		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" lucee/start
-		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" lucee/stop
+		sed -i "s/jetty.port=8888/jetty.port=$SERVER_PORT/g" lucee/start.sh
+		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" lucee/start.sh
+		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" lucee/stop.sh
 		;;
 	start|stop)
 		;;
