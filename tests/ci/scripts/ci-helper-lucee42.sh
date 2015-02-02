95b82bdf4063d282dc96c4afcc572dbe17e459d6
Matt Gifford <me@mattgifford.co.uk>
2015-02-02 13:22:26 +0000

    Revised L testing process

--- tests/ci/scripts/ci-helper-lucee42.sh
+++ tests/ci/scripts/ci-helper-lucee42.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 case $1 in
 	start)
-		CONTROL_SCRIPT='start.sh'
+		CONTROL_SCRIPT='lucee-4.5.0.042-express/start.sh'
 		;;
 	stop)
-		CONTROL_SCRIPT='stop.sh'
+		CONTROL_SCRIPT='lucee-4.5.0.042-express/stop.sh'
 		;;
 esac
 
-PLATFORM_DIR="lucee"
-WEBROOT="webapps/ROOT"
+PLATFORM_DIR="lucee-4.5.0.042-express"
+WEBROOT="lucee-4.5.0.042-express/webapps/ROOT"
 MY_DIR=`dirname $0`
 source $MY_DIR/ci-helper-base.sh $1 $2
 
 case $1 in
 	install)
-		chmod a+x start.sh
-		chmod a+x stop.sh
+		chmod a+x lucee-4.5.0.042-express/start.sh
+		chmod a+x lucee-4.5.0.042-express/stop.sh
 
-		sed -i "s/jetty.port=8888/jetty.port=$SERVER_PORT/g" start.sh
-		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" start.sh
-		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" stop.sh
+		sed -i "s/jetty.port=8888/jetty.port=$SERVER_PORT/g" lucee-4.5.0.042-express/start.sh
+		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" lucee-4.5.0.042-express/start.sh
+		sed -i "s/STOP.PORT=8887/STOP.PORT=$STOP_PORT/g" lucee-4.5.0.042-express/stop.sh
 		;;
 	start|stop)
 		;;
